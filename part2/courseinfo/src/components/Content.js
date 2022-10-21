17f01790927dee23661957108e4a1a51e90e8aca
Hunter Chambers <chambersh1129@gmail.com>
2022-10-21 07:12:09 -0400

    completed exercise 2.1 Course Info Step 6

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,19 @@
+const Part = ({ part }) => {
+    return (
+        <>
+            <p>{part.name} {part.exercises}</p>
+        </>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            {parts.map(part =>
+                <Part key={part.id} part={part} />
+            )}
+        </>
+    )
+}
+
+export default Content
