374cb0ea749354dfa6abe747a1012f30054bac80
Hunter Chambers <chambersh1129@gmail.com>
2022-10-23 09:42:34 -0400

    completed exercise 2.5, adding the rest

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,29 @@
-import CourseTitle from "./CourseTitle"
-import Content from "./Content"
+
+const Part = ({ part }) => {
+    return (
+        <>
+            <p>{part.name} {part.exercises}</p>
+        </>
+    )
+}
+
+const SumOfExercises = ({ parts }) => {
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
+    return (
+        <>
+            <p><strong>total of {total} exercises</strong></p>
+        </>
+    )
+}
 
 const Course = ({ course }) => {
     return (
         <>
-            <CourseTitle text={course.name} />
-            <Content parts={course.parts} />
+            <h2>{course.name}</h2>
+            {course.parts.map(part =>
+                <Part key={part.id} part={part} />
+            )}
+            <SumOfExercises parts={course.parts} />
         </>
     )
 }
--- part2/courseinfo/src/components/CourseTitle.js
+++
@@ -1,9 +0,0 @@
-const CourseTitle = ({ text }) => {
-    return (
-        <>
-            <h2>{text}</h2>
-        </>
-    )
-}
-
-export default CourseTitle
